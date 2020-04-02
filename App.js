a86597d4a2085196df436aa29685a5b321861c69
unknown <bu8886679@BSVDI.COM>
2020-04-02 17:16:38 +0900

    ddd

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, TouchableOpacity, TextInput, AsyncStorage } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, AsyncStorage, Alert } from 'react-native';
 import LoginHandle from './components/LoginHandle';
 
 export default class App extends React.Component {
@@ -21,13 +21,24 @@ export default class App extends React.Component {
   }
 
   onSave = async () => {
+
+    if(!this.state.id){
+      Alert.alert("아이디를 입력하세요");
+      return;
+    }
+
+    if(!this.state.password){
+      Alert.alert("아이디를 입력하세요");
+      return;
+    }
+
     try{
       await AsyncStorage.setItem("SAVE_ID", this.state.id);
       await AsyncStorage.setItem("SAVE_PW", this.state.password);
     }
     catch(err){
       alert(err);
-      return false;
+      return;
     }
 
     this.onSubmit();
@@ -58,7 +69,7 @@ export default class App extends React.Component {
       }
     }
     catch(err){
-      alert(err)
+      Alert.alert(err)
     }
   }
 
@@ -66,21 +77,71 @@ export default class App extends React.Component {
     return (
         this.state.submit ?
         <LoginHandle id={this.state.id} password={this.state.password} sumit={this.state.submit} />
-        : <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
-        <TextInput
-          placeholder="아이디"
-          onChangeText={text => this.setState({ id: text })}
-        />
-        <TextInput
-          secureTextEntry={true}
-          placeholder="패스워드"
-          onChangeText={text => this.setState({ password: text })}
-        />
-        <TouchableOpacity onPress={() => this.onSave()}>
-          <Text>로그인</Text>
-        </TouchableOpacity>
+        : <View style={styles.container}>
+          <View style={styles.wrap}>
+            <View style={{ marginBottom: 20}}>
+              <Text style={styles.title}>NCIN MEMBERS AUTO LOGIN</Text>
+            </View>
+            <View style={{flex:1, marginBottom:5}}>
+              <TextInput
+                placeholder="아이디"
+                onChangeText={text => this.setState({ id: text })}
+                style={styles.input}
+              />
+            </View>
+            <View style={{flex:1, marginBottom:5}}>
+              <TextInput
+                secureTextEntry={true}
+                placeholder="패스워드"
+                onChangeText={text => this.setState({ password: text })}
+                style={styles.input}
+              />
+            </View>
+            <View style={{flex:1}}>
+              <TouchableOpacity onPress={() => this.onSave()} style={styles.submit}>
+                <Text style={{color:"#fff"}}>로그인</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
       </View>
     )
   }
   
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#e9e9e9",
+    padding:15
+  },
+  wrap: {
+    padding: 30,
+    borderRadius: 30,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    backgroundColor: "#fff"
+  },
+  title: {
+    fontSize: 30,
+    color: "#333",
+    lineHeight: 32,
+    textAlign:"center"
+  },
+  input: {
+    height:50,
+    paddingHorizontal: 10,
+    borderWidth:1,
+    borderColor: "#ccc",
+    borderRadius: 5
+  },
+  submit: {
+    height: 50,
+    borderRadius: 5,
+    backgroundColor: "#5E6898",
+    justifyContent: "center",
+    alignItems: "center"
+  }
+})
--- components/LoginHandle.js
+++ components/LoginHandle.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import { Alert } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default class LoginHandle extends React.Component {
 
+    // injectLogin = (id, password) => {
+    //     return `(function(id, password){
+    //         setTimeout(() => {
+    //             try{
+    //                 fn_goOffice();
+    //             } catch(err) {
+    //                 alert('자동로그인 실패하였습니다.');
+    //             }
+    //             window.close();
+    //         });
+    //         document.getElementById("userid").value = id;
+    //         document.getElementById("password").value = password;
+    //         document.getElementById("loginVO").submit();
+    //     }('${id}','${password}'))`
+    // }
+
     injectLogin = (id, password) => {
         return `(function(id, password){
             setTimeout(() => {
-                try{
-                    fn_goOffice();
-                } catch(err) {
-                    alert('자동로그인 실패하였습니다.');
-                }
-                window.close();
-            });
+                window.ReactNativeWebView.postMessage("되는거가마는거가");
+            }, 3000);
             document.getElementById("userid").value = id;
             document.getElementById("password").value = password;
             document.getElementById("loginVO").submit();
@@ -25,6 +37,8 @@ export default class LoginHandle extends React.Component {
             <WebView 
                 source={{uri:'http://gw.ncin.co.kr/Login.do'}}
                 injectedJavaScript={this.injectLogin(id, password)}
+                onMessage={(event) => Alert.alert(event.nativeEvent.data)}
+                ref={(webView) => this.webView = webView}
                 style={{width:"100%", height: "100%"}}
             />
         )
